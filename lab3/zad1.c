7bca360822d2590795668a7791ec99dbc4edc884
wwojtasq <61745665+wwojtasq@users.noreply.github.com>
2020-11-13 20:30:26 +0100

    Add files via upload

---
+++ lab3/zad1.c
@@ -0,0 +1,14 @@
+//Wojciech Kubicki 241170
+
+//Program:
+#include <iostream>
+using namespace std;
+
+int main () {
+	for ( ; ; ) {
+		printf ("1\n");
+	}	
+return 0;
+}
+
+//Użycie funkcji sleep powoduje zniknięcie programu ze szczytu listy.
